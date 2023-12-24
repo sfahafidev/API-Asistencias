eb024b900ace1331654553b3ca4fa75f2b314a7d
sfahafi <sfahafidev@gmail.com>
2023-12-24 00:32:12 -0300

    Primer commit

---
+++ src/main/java/com/assist/control/ControlAsistenciasApplication.java
@@ -0,0 +1,13 @@
+package com.assist.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlAsistenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlAsistenciasApplication.class, args);
+	}
+
+}
